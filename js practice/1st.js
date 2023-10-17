4a90e8224e20527c54db6a077555154ff6e351e3
mehedi493660 <144321420+mehedi493660@users.noreply.github.com>
2023-10-17 13:24:06 +0600

    Add files via upload

---
+++ js practice/1st.js
@@ -0,0 +1,43 @@
+// var money = 500;
+// var apple = 10;
+// var price = 20;
+// var x = apple * price;
+// var orrange = 8;
+// var price = 15;
+// var y = orrange * price;
+// var allprice = x+y;
+// var back = money - allprice;
+// console.log(back);  
+
+
+// var numbers = [12,24,31,64,52,76,34534]
+
+// //  console.log(numbers.length)
+// //  console.log(numbers)
+// // numbers[5]=68;
+// // numbers.pop()
+// // numbers.push(500);
+// numbers
+// console.log(numbers);
+
+
+// var number_of_apple = 17;
+// var price_of_apple = 391;
+
+// var single_apple = price_of_apple / number_of_apple;
+
+// console.log(single_apple);
+
+// var number_of_famely=324;
+// var amount = 250;
+
+// var all_amount = number_of_famely*amount;
+// console.log(all_amount);
+
+
+// var input = 20;
+// var per = 25;
+// var per_cal = per / 100;
+// var result =input*per_cal;
+
+// console.log(result);
